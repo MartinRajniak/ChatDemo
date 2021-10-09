9ae82885b26f49d0dee18b76915d33b48c210098
Martin Rajniak <rajniak.m@gmail.com>
2021-10-09 10:02:44 +0200

    Add Toolbar
    With dummy profile icon, title and action icon.

--- app/src/main/java/eu/rajniak/chat/ChatUI.kt
+++ app/src/main/java/eu/rajniak/chat/ChatUI.kt
@@ -1,25 +1,45 @@
 package eu.rajniak.chat
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.insets.LocalWindowInsets
 import com.google.accompanist.insets.navigationBarsWithImePadding
-import com.google.accompanist.insets.statusBarsPadding
+import com.google.accompanist.insets.rememberInsetsPaddingValues
 import eu.rajniak.chat.ui.theme.ChatDemoTheme
 
 @Composable
 fun ChatUI() {
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
         Column(Modifier.fillMaxSize()) {
-            Toolbar(
-                modifier = Modifier.statusBarsPadding()
-            )
+            Toolbar()
             MessageList(
                 modifier = Modifier.weight(1f)
             )
@@ -32,18 +52,85 @@ fun ChatUI() {
 
 @Composable
 fun Toolbar(
-    modifier: Modifier
+    navigationIcon: @Composable (() -> Unit),
+    title: @Composable () -> Unit,
+    actions: @Composable RowScope.() -> Unit = {},
 ) {
-    Box(modifier = modifier) {
-        Text("Toolbar")
+    TopAppBar(
+        contentPadding = rememberInsetsPaddingValues(
+            insets = LocalWindowInsets.current.statusBars,
+            applyStart = true,
+            applyTop = true,
+            applyEnd = true,
+        ),
+        backgroundColor = Color.White,
+    ) {
+        Row(
+            modifier = TitleIconModifier,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            CompositionLocalProvider(
+                LocalContentAlpha provides ContentAlpha.high,
+                content = navigationIcon
+            )
+        }
+
+        Row(
+            Modifier
+                .fillMaxHeight()
+                .weight(1f),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            title()
+        }
+
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Row(
+                Modifier.fillMaxHeight(),
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically,
+                content = actions
+            )
+        }
     }
 }
+
+@Composable
+fun Toolbar() {
+    Toolbar(
+        navigationIcon = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+            }
+        },
+        title = {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Profile picture"
+            )
+            Spacer(modifier = Modifier.padding(end = 4.dp))
+            Text(text = "Sarah")
+        },
+        actions = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.MoreVert, contentDescription = "More")
+            }
+        }
+    )
+}
+
+private val AppBarHorizontalPadding = 4.dp
+
+// Start inset for the title when there is a navigation icon provided
+private val TitleIconModifier = Modifier
+    .fillMaxHeight()
+    .width(56.dp - AppBarHorizontalPadding)
 
 @Composable
 fun MessageList(
     modifier: Modifier
 ) {
-    Box(modifier = modifier){
+    Box(modifier = modifier) {
         Text("MessageList")
     }
 }
@@ -52,7 +139,7 @@ fun MessageList(
 fun TextEntryBox(
     modifier: Modifier
 ) {
-    Box(modifier = modifier){
+    Box(modifier = modifier) {
         Text("TextEntryBox")
     }
 }
