83050a30c9ea0409b7f347aeb68cb175dec1af33
Martin Rajniak <rajniak.m@gmail.com>
2021-10-09 12:35:03 +0200

    Update system bars to be translucent
    Fixes invisible status bar icons.

--- app/src/main/java/eu/rajniak/chat/MainActivity.kt
+++ app/src/main/java/eu/rajniak/chat/MainActivity.kt
@@ -3,8 +3,12 @@ package eu.rajniak.chat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat
 import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import eu.rajniak.chat.ui.theme.ChatDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,6 +18,12 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
+            // Update the system bars to be translucent
+            val systemUiController = rememberSystemUiController()
+            val useDarkIcons = MaterialTheme.colors.isLight
+            SideEffect {
+                systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = useDarkIcons)
+            }
             ChatDemoTheme {
                 ProvideWindowInsets {
                     ChatUI()
